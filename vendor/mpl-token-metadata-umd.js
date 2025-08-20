0c9630f7cf9c8899aaed0d4719c14d0ddb9b2271
Xpective <81888420+Xpective@users.noreply.github.com>
2025-08-20 20:10:51 +0000

    upgrade

---
+++ vendor/mpl-token-metadata-umd.js
@@ -0,0 +1,5 @@
+/* esm.sh - @metaplex-foundation/mpl-token-metadata@3.4.0 */
+import "/@metaplex-foundation/umi@^1.2.0/serializers?target=es2020";
+import "/@metaplex-foundation/umi@^1.2.0?target=es2020";
+export * from "/@metaplex-foundation/mpl-token-metadata@3.4.0/es2020/mpl-token-metadata.bundle.mjs";
+export { default } from "/@metaplex-foundation/mpl-token-metadata@3.4.0/es2020/mpl-token-metadata.bundle.mjs";
